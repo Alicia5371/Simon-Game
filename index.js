b7cd4c92ebeb4b7298685e870a1f09a14c8ecec6
Alicia <AliciaMBellamy@gmail.com>
2026-04-23 00:36:24 -0400

    Created functions using squery to play the sounds when the button it clicked

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// $(".green").css("background-color", "pink");
+//!Click function that plays the sound
+$(".red").click(function (e) {
+  e.preventDefault();
+  let audio = new Audio("sounds/red.mp3");
+  audio.play();
+});
+
+$(".green").click(function (e) {
+  e.preventDefault();
+  let audio = new Audio("sounds/green.mp3");
+  audio.play();
+});
+
+$(".yellow").click(function (e) {
+  e.preventDefault();
+  let audio = new Audio("sounds/yellow.mp3");
+  audio.play();
+});
+
+$(".blue").click(function (e) {
+  e.preventDefault();
+  let audio = new Audio("sounds/blue.mp3");
+  audio.play();
+});
+
+function nextSequence() {}
