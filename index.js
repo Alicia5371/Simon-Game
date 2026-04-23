a40ef8608d451e7a887e5675ab4741427b7bcc61
Alicia <AliciaMBellamy@gmail.com>
2026-04-23 01:55:32 -0400

    Added on to the function to pull from the array what each random numbers color would be

--- index.js
+++ index.js
@@ -1,26 +1,37 @@
-//!Click function that plays the sound
-$(".red").click(function (e) {
-  e.preventDefault();
-  let audio = new Audio("sounds/red.mp3");
-  audio.play();
-});
+// $(".red").click(function (e) {
+//   e.preventDefault();
+//   let audio = new Audio("sounds/red.mp3");
+//   audio.play();
+// });
 
-$(".green").click(function (e) {
-  e.preventDefault();
-  let audio = new Audio("sounds/green.mp3");
-  audio.play();
-});
+// $(".green").click(function (e) {
+//   e.preventDefault();
+//   let audio = new Audio("sounds/green.mp3");
+//   audio.play();
+// });
 
-$(".yellow").click(function (e) {
-  e.preventDefault();
-  let audio = new Audio("sounds/yellow.mp3");
-  audio.play();
-});
+// $(".yellow").click(function (e) {
+//   e.preventDefault();
+//   let audio = new Audio("sounds/yellow.mp3");
+//   audio.play();
+// });
 
-$(".blue").click(function (e) {
-  e.preventDefault();
-  let audio = new Audio("sounds/blue.mp3");
-  audio.play();
-});
+// $(".blue").click(function (e) {
+//   e.preventDefault();
+//   let audio = new Audio("sounds/blue.mp3");
+//   audio.play();
+// });
 
-function nextSequence() {}
+//let buttonColors = ["red", "blue", "green", "yellow"];
+
+// let randomChosenColor = buttonColors[randomNumber];
+
+let gamePattern = [];
+
+function nextSequence() {
+  let buttonColors = ["red", "blue", "green", "yellow"];
+  let randomNumber = Math.floor(Math.random() * 4);
+  let randomChosenColor = buttonColors[randomNumber];
+  return randomChosenColor;
+}
+console.log(nextSequence());
