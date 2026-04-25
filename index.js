21b00ea673bffe096549b0ab92184c3d6979a3ee
Alicia <AliciaMBellamy@gmail.com>
2026-04-24 21:56:10 -0400

    Added back the sounds for each button and now an flahs animation using jQuery whenever a button is presses

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-// $(".red").click(function (e) {
-//   e.preventDefault();
-//   let audio = new Audio("sounds/red.mp3");
-//   audio.play();
-// });
-
-// $(".green").click(function (e) {
-//   e.preventDefault();
-//   let audio = new Audio("sounds/green.mp3");
-//   audio.play();
-// });
-
-// $(".yellow").click(function (e) {
-//   e.preventDefault();
-//   let audio = new Audio("sounds/yellow.mp3");
-//   audio.play();
-// });
-
-// $(".blue").click(function (e) {
-//   e.preventDefault();
-//   let audio = new Audio("sounds/blue.mp3");
-//   audio.play();
-// });
-
-//let buttonColors = ["red", "blue", "green", "yellow"];
-
-// let randomChosenColor = buttonColors[randomNumber];
+let buttonColors = ["red", "blue", "green", "yellow"];
 
 let gamePattern = [];
 
 function nextSequence() {
-  let buttonColors = ["red", "blue", "green", "yellow"];
   let randomNumber = Math.floor(Math.random() * 4);
   let randomChosenColor = buttonColors[randomNumber];
-  return randomChosenColor;
+  gamePattern.push(randomChosenColor);
 }
-console.log(nextSequence());
+
+$(".red").click(function (e) {
+  e.preventDefault();
+  $(".red").fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/red.mp3");
+  audio.play();
+});
+
+$(".green").click(function (e) {
+  e.preventDefault();
+  $(".green").fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/green.mp3");
+  audio.play();
+});
+
+$(".yellow").click(function (e) {
+  e.preventDefault();
+  $(".yellow").fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/yellow.mp3");
+  audio.play();
+});
+
+$(".blue").click(function (e) {
+  e.preventDefault();
+  $(".blue").fadeOut(100).fadeIn(100);
+  let audio = new Audio("sounds/blue.mp3");
+  audio.play();
+});
